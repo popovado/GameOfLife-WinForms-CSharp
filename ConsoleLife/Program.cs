c43fa6d09c0f5dd3f9d07bf44d4715c8db86f13e
Полина Попова <polinap.2017@bk.ru>
2024-09-13 17:58:44 +0300

    Перенос игры в консоль

---
+++ ConsoleLife/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLife
+{
+    class Program
+    {
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int MAXIMIZE = 3;
+
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            ShowWindow(GetConsoleWindow(), MAXIMIZE);
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+
+            var gameEngine = new GameEngine
+                (
+                rows:168,
+                columns:630,
+                density: 2
+                );
+
+            while (true)
+            {
+                Console.Title =gameEngine.CurrentGeneration.ToString();
+
+                var field = gameEngine.GetCurrentGeneration();
+
+                for (int y = 0; y < field.GetLength(1); y++)
+                {
+                    var str = new char[field.GetLength(0)];
+
+                    for (int x = 0; x < field.GetLength(0); x++)
+                    {
+                        if (field[x, y])
+                            str[x] = '#';
+                        else
+                            str[x] = ' ';
+                    }
+                    Console.WriteLine(str);
+                }
+                Console.SetCursorPosition (0, 0);
+                gameEngine.NextGeneration();
+            }
+
+        }
+    }
+}
